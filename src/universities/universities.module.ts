c07965db5b7089df10bb3d5e72626683ab057561
Andrei-Majada <asmajada@inf.ufpel.edu.br>
2023-04-05 23:14:25 -0300

    feat: creating and importing University schema

--- src/universities/universities.module.ts
+++ src/universities/universities.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UniversitiesController } from './universities.controller';
 import { UniversitiesService } from './universities.service';
+import { University, UniversitySchema } from './university.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: University.name, schema: UniversitySchema },
+    ]),
+  ],
   controllers: [UniversitiesController],
   providers: [UniversitiesService],
   exports: [UniversitiesService],
