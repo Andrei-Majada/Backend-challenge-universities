0d8497f2c4d0f350f85c3ae3f1f25df6ed5e4f7b
Andrei-Majada <asmajada@inf.ufpel.edu.br>
2023-04-06 10:55:25 -0300

    feat: adding create user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { User } from './user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @HttpCode(201)
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const { name, email } = createUserDto;
+
+      const findUser = await this.userModel.findOne({ name, email }).exec();
+
+      if (findUser) {
+        throw new HttpException(
+          `User already exists in database.`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      const user = new this.userModel(createUserDto);
+      return await user.save();
+    } catch (error) {
+      throw new HttpException(
+        `Error while creating user. Error: ${error}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
