64f302e86b2816aac75ff3549999727c141e0bfb
Andrei-Majada <asmajada@inf.ufpel.edu.br>
2023-04-05 23:13:45 -0300

    feat: importing mongoDB to main app and gitignore mongoDB files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UniversitiesModule } from './universities/universities.module';
 
+const MONGO_URL = 'mongodb://localhost:27017/university';
+
 @Module({
   imports: [
+    MongooseModule.forRoot(MONGO_URL),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
