249135782ef3e51e0aadbf1d40f2f3d3eac305f1
Andrei-Majada <asmajada@inf.ufpel.edu.br>
2023-04-05 23:34:04 -0300

    feat: adding create university and createDTO

--- src/universities/universities.controller.ts
+++ src/universities/universities.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get, HttpCode } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { UniversitiesService } from './universities.service';
 import { University } from './university.schema';
+import { CreateUniversityDto } from './dto/create-university.dto';
 
 @Controller('/universities')
 export class UniversitiesController {
   constructor(private readonly universitiesService: UniversitiesService) {}
+
+  @Post()
+  @HttpCode(201)
+  create(
+    @Body() createUniversityDto: CreateUniversityDto,
+  ): Promise<University> {
+    return this.universitiesService.create(createUniversityDto);
+  }
 
   @Get()
   @HttpCode(200)
--- src/universities/universities.service.ts
+++ src/universities/universities.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { University, UniversityDocument } from './university.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CreateUniversityDto } from './dto/create-university.dto';
 
 @Injectable()
 export class UniversitiesService {
@@ -9,6 +10,18 @@ export class UniversitiesService {
     @InjectModel(University.name)
     private universityModel: Model<UniversityDocument>,
   ) {}
+
+  async create(createUniversityDto: CreateUniversityDto): Promise<University> {
+    try {
+      const university = new this.universityModel(createUniversityDto);
+      return university.save();
+    } catch (error) {
+      throw new HttpException(
+        `Error while creating university. Error: ${error}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 
   async findAll(): Promise<University[]> {
     try {
